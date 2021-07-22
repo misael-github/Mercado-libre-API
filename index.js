767db924a61cb99f0376f76cd2d4ae3d7c7dcae4
Misael <dmisael2000@gmail.com>
2021-07-21 23:54:11 -0300

    add link to ML and changes price-condition-vendidos

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+
+
 function mostrarResultados(results) {
   // Recibe la data que le pasamos en main
   const contenedor = document.querySelector(".results");
   const template = document.getElementById("result-item-template");
 
+
   for (const resultados of results) {
+    // resultados es la variable que creamos y results es el parametro de la funcion (mostrarResultados) que me trae la data
     // console.log(resultados);
     const titleEl = template.content.querySelector(".result-item-title");
     titleEl.textContent = resultados.title;
+
+    const imgAltEl = template.content.querySelector(".result-item-img")
+    imgAltEl.alt = resultados.title
 
     // const resultsEl = document.querySelector(".results-count")
     // resultsEl.textContent = resultadosArticle
@@ -17,14 +24,23 @@ function mostrarResultados(results) {
     const priceEl = template.content.querySelector(".result-item-price");
     priceEl.textContent = `$ ${resultados.price}`;
 
-    const soldEl = template.content.querySelector(".result-item-sell-count-num")
-    soldEl.textContent =`Vendidos: ${resultados.sold_quantity}` 
+    const soldEl = template.content.querySelector(
+      ".result-item-sell-count-num"
+    );
+    soldEl.textContent = `Vendidos: ${resultados.sold_quantity}`;
 
-    const conditionEl = template.content.querySelector(".result-item-condition");
+    const conditionEl = template.content.querySelector(
+      ".result-item-condition"
+    );
     conditionEl.textContent = resultados.condition;
-
+    
+    const linkmlEl = template.content.querySelector(".result-item-link-ml")
+    linkmlEl.href = resultados.permalink
+  
     const clone = document.importNode(template.content, true);
     contenedor.appendChild(clone); // A contenedor se agrega clone
+
+    
   }
  
 }
@@ -40,8 +56,9 @@ function main() {
     fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${palabraABuscar}`)
       .then((response) => response.json())
       .then((data) => mostrarResultados(data.results)); // results es el objeto del array de toda la respuesta que tiene los resultados que nesecitamos
-      const contenedor = document.querySelector(".results");
-      contenedor.textContent =""
+    
+    const contenedor = document.querySelector(".results");
+    contenedor.textContent = "";
   });
 }
 main();
